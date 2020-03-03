3b164ad03392b61d3e03474556fbdeaa178a906a
Caio Rodrigues <rfcaio90@gmail.com>
2020-03-03 14:14:27 -0300

    change project structure

--- karma.conf.js
+++ karma.conf.js
@@ -11,16 +11,16 @@ module.exports = function (config) {
       'src/layout/layout.module.js',
       'src/layout/panel/panel.module.js',
       'src/layout/panel/panel.directive.js',
+      'src/layout/panel/panel.directive.spec.js',
       'src/photo/photo.module.js',
       'src/photo/photo.controller.js',
+      'src/photo/photo.controller.spec.js',
       'src/photo/photo.service.js',
+      'src/photo/photo.service.spec.js',
       'src/photo/photo-create.controller.js',
+      'src/photo/photo-create.controller.spec.js',
       'src/photo/photo-update.controller.js',
-      'spec/photo/photo.controller.spec.js',
-      'spec/photo/photo.service.spec.js',
-      'spec/photo/photo-create.controller.spec.js',
-      'spec/photo/photo-update.controller.spec.js',
-      'spec/layout/panel/panel.directive.spec.js'
+      'src/photo/photo-update.controller.spec.js'
     ],
     frameworks: ['jasmine']
   });
